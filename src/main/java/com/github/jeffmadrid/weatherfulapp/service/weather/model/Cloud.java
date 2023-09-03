3cce8bd20c107ed62a979b55cd50974502650776
Jefferson Madrid <jefferson.imadrid@gmail.com>
2023-09-03 15:45:25 +1000

    Clean up: unused files due to moving files around

--- src/main/java/com/github/jeffmadrid/weatherfulapp/service/weather/model/Cloud.java
+++
@@ -1,9 +0,0 @@
-package com.github.jeffmadrid.weatherfulapp.service.weather.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Cloud(
-    Integer all
-) {
-}
